2185140491f5c46f7ad1d808c56164d39efbccf6
Apurba <apurva.gouda@gmail.com>
2022-12-17 21:09:46 +0530

    Migrated feeds and video views to ConstraintLayout

--- app/src/main/java/com/sarvagya/android/ui/home/videos/VideosFragment.kt
+++ app/src/main/java/com/sarvagya/android/ui/home/videos/VideosFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.divider.MaterialDividerItemDecoration
 import com.sarvagya.android.databinding.FragmentVideosBinding
 
 class VideosFragment(private val listener: VideoAdapterOnClickListener) : Fragment() {
@@ -26,6 +28,7 @@ class VideosFragment(private val listener: VideoAdapterOnClickListener) : Fragme
     private fun setUpVideosList() {
         videosBinding.videosRV.apply {
             layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+            addItemDecoration(MaterialDividerItemDecoration(this.context, RecyclerView.VERTICAL))
             adapter = videosAdapter
         }
     }
